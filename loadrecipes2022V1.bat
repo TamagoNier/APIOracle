7226b57cca725359fa4e80930d41365f574334fc
Egor Gututui <102786178+TamagoNier@users.noreply.github.com>
2024-02-01 18:23:44 +0100

    premier commit :)

---
+++ loadrecipes2022V1.bat
@@ -0,0 +1,22 @@
+call symfony composer require annotations
+call symfony composer require maker --dev 
+call symfony composer require symfony/apache-pack --dev
+call symfony composer require twig
+call symfony composer require logger 
+call symfony composer require profiler --dev
+call symfony composer require debug --dev  
+call symfony composer require symfony/form
+call symfony composer require symfony/validator
+call symfony composer require symfony/orm-pack
+call symfony composer require symfony/mime
+call symfony composer require symfony/asset
+call symfony composer require symfony/mailer
+call symfony composer require sensio/framework-extra-bundle 
+call symfony composer require symfony/security-bundle
+# call composer require symfony/webpack-encore-bundle
+
+
+
+
+
+
